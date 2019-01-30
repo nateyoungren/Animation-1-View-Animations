17e08b0440a1dcbf3671d8464a0b8aa8c941d082
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-30 12:41:14 -0800

    Setup VC with IBAction.

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Nathanael Youngren on 1/30/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func toggle(_ sender: UIBarButtonItem) {
+        let shouldScramble = sender.isEnabled
+        
+        if shouldScramble {
+            //
+        } else {
+            //
+        }
+    }
+    
+
+}
+
