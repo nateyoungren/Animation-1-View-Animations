b45dbb30bf73855e4a8535cce1074be8ec8df4a5
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-30 13:56:29 -0800

    Create views.

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        createViews()
     }
     
     
@@ -26,6 +26,87 @@ class ViewController: UIViewController {
         }
     }
     
+    func createViews() {
+        
+        let lLabel = UILabel()
+        lLabel.text = "L"
+        lLabel.font = UIFont(name: "Avenir-Light", size: 30)
+        lLabel.sizeToFit()
+        lLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(lLabel)
+        
+        let aLabel = UILabel()
+        aLabel.text = "a"
+        aLabel.font = UIFont(name: "Avenir-Light", size: 30)
+        aLabel.sizeToFit()
+        aLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(aLabel)
+        
+        let mLabel = UILabel()
+        mLabel.text = "m"
+        mLabel.font = UIFont(name: "Avenir-Light", size: 30)
+        mLabel.sizeToFit()
+        mLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mLabel)
+        
+        let bLabel = UILabel()
+        bLabel.text = "b"
+        bLabel.font = UIFont(name: "Avenir-Light", size: 30)
+        bLabel.sizeToFit()
+        bLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bLabel)
+        
+        let dLabel = UILabel()
+        dLabel.text = "d"
+        dLabel.font = UIFont(name: "Avenir-Light", size: 30)
+        dLabel.sizeToFit()
+        dLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(dLabel)
+        
+        let a2Label = UILabel()
+        a2Label.text = "a"
+        a2Label.font = UIFont(name: "Avenir-Light", size: 30)
+        a2Label.sizeToFit()
+        a2Label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(a2Label)
+
+        let image = UIImage(named: "Lambda_Logo_Full")
+        let logo = UIImageView(image: image)
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.setContentHuggingPriority(.init(1000), for: .horizontal)
+        view.addSubview(logo)
+        
+        let textStackView = UIStackView()
+        view.addSubview(textStackView)
+        textStackView.axis = .horizontal
+        textStackView.distribution = .equalSpacing
+        textStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        textStackView.addArrangedSubview(lLabel)
+        textStackView.addArrangedSubview(aLabel)
+        textStackView.addArrangedSubview(mLabel)
+        textStackView.addArrangedSubview(bLabel)
+        textStackView.addArrangedSubview(dLabel)
+        textStackView.addArrangedSubview(a2Label)
+        
+        let outerStackView = UIStackView()
+        view.addSubview(outerStackView)
+        outerStackView.axis = .vertical
+        outerStackView.distribution = .equalSpacing
+        outerStackView.spacing = 8
+        outerStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        outerStackView.addArrangedSubview(textStackView)
+        outerStackView.addArrangedSubview(logo)
+        
+        NSLayoutConstraint.activate([
+            outerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            outerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30),
+            outerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            ])
+        
+        
+    }
+    
 
 }
-
